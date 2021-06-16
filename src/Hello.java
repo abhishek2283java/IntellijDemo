e13d43677d790e1098848288adce575fc8db0f91
abhishek <abhishek2283@hotmail.com>
2021-06-16 15:36:27 +0200

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+
+        String initMessage = "This is first message";
+        System.out.println(initMessage);
+    }
+}
